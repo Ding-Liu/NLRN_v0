f48d154ba34599a39166b559059c3db8e73ab182
Ding Liu <dingliu2@ifp-06.ifp.uiuc.edu>
2019-01-16 01:30:14 -0600

    minor changes

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ MODEL_FILE="$TRAIN_DIR/$MODEL_NAME-$DATA_NAME-400000"
 # Test on Set12 or BSD68
 IMAGE_FOLDER="./data/Set12/"
 #IMAGE_FOLDER="./data/BSD68/"
-NOISY_IMAGE_FOLDER="/ws/ifp-06_1/dingliu2/data/Denoising/Set12/sigma$SIGMA/"
+NOISY_IMAGE_FOLDER="./data/Set12/sigma$SIGMA/"
 ARGS="--data_name=$DATA_NAME --image_folder=$IMAGE_FOLDER --noisy_image_folder=$NOISY_IMAGE_FOLDER --model_name=$MODEL_NAME --model_file=$MODEL_FILE --sigma=$SIGMA --patch_size=$PATCH_SIZE --state_num=$STATE_NUM"
 
 echo $ARGS
